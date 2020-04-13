a65e9286e067facda35c4f724922cb76654ae366
Mario <marioperozo@gmail.com>
2020-04-13 11:53:39 -0400

    Palindrome Spotter

--- textmanipulatio/ViewController.swift
+++ textmanipulatio/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var originalLabel: UILabel!
+    @IBOutlet weak var reversedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func removeSpecialCharsFromString(text: String) -> String {
+        let okayChars : Set<Character> =
+            Set("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLKMNOPQRSTUVWXYZ1234567890")
+        return String(text.filter {okayChars.contains($0) })
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder();
+        return true;
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let word: String = textField.text!.lowercased();
+        var formattedWord = word.replacingOccurrences(of: " ", with: "")
+        formattedWord = removeSpecialCharsFromString(text: formattedWord);
+        var drow = word.replacingOccurrences(of: " ", with: "");
+        drow = removeSpecialCharsFromString(text: drow);
+        drow = String(formattedWord.reversed());
+        
+        originalLabel.text = formattedWord
+        reversedLabel.text = drow;
+        
+        if formattedWord == drow {
+            label.text = "The word or sentence is a Palindrome"
+        }
+        else {
+            label.text = "The word or sentence is NOT a Palindrome"
+        }
+        
+        
+    }
 }
-
